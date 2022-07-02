a4a576c4efb829dde944e34da406fce18316aea2
mohit-11011 <95518355+mohit-11011@users.noreply.github.com>
2022-07-02 12:19:42 +0530

    Time: 3 ms (63.69%), Space: 8.7 MB (85.49%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int carry=0;
-        int added=0;
         for(int i=digits.size()-1;i>=0;i--)
         {
             int temp=digits[i];
