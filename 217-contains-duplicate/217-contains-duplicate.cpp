87d7c8d3957d83c5925a5aab6165c808b1345eba
mohit-11011 <95518355+mohit-11011@users.noreply.github.com>
2022-07-05 22:35:32 +0530

    Time: 172 ms (25.89%), Space: 55.3 MB (6.24%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -4,10 +4,6 @@ public:
         unordered_set<int> s(nums.begin(),nums.end());
         int n=nums.size();
         int x=s.size();
-        if(n-x>0)
-        {
-            return true;
-        }
-        return false;
+       return n-x>0;
     }
 };
