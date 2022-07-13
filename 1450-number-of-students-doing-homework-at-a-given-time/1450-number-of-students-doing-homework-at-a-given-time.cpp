2cc2075fb5138c10edb9b0b2f5432a5b839fe1ef
mohit-11011 <95518355+mohit-11011@users.noreply.github.com>
2022-07-14 00:58:51 +0530

    Time: 4 ms (68.96%), Space: 10.8 MB (83.69%) - LeetHub

--- 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.cpp
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
-        int n=startTime.size();
         int ans=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<startTime.size();i++)
         {
             if(queryTime>=startTime[i] && queryTime<=endTime[i])
                 ans++;
